b859ff70cb55ccb18ba58892e54a64bebb5637b2
yaseenabdulsatar <112056637+yaseenabdulsatar@users.noreply.github.com>
2024-03-09 21:34:28 +0300

    Update setup_webserver.sh

--- solutions/wordpress-azure-vm-v1/scripts/setup_webserver.sh
+++ solutions/wordpress-azure-vm-v1/scripts/setup_webserver.sh
@@ -53,13 +53,6 @@ echo $phpVersion          >> /tmp/vars.txt
 
 check_fileServerType_param $fileServerType
 
-mkdir -p /etc/nginx/sites-enabled/
-cat > /etc/nginx/sites-enabled/default << EOF
-upstream backend {
-        server unix:/run/php/php${PhpVer}-fpm.sock fail_timeout=1s;
-        server unix:/run/php/php${PhpVer}-fpm-backup.sock backup;
-} 
-EOF
   # make sure the system does automatic update
   # apt-get -y update
   # TODO: ENSURE THIS IS CONFIGURED CORRECTLY
@@ -320,12 +313,6 @@ EOF
         setup_html_local_copy_cron_job
      fi   
   fi
-cat > /etc/nginx/sites-enabled/default << EOF
-upstream backend {
-        server unix:/run/php/php${PhpVer}-fpm.sock fail_timeout=1s;
-        server unix:/run/php/php${PhpVer}-fpm-backup.sock backup;
-} 
-EOF
   config_all_sites_on_vmss $htmlLocalCopySwitch $httpsTermination
 
   # php config 
