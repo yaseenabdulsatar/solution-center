039520c41339ea0b0b11948b5029c9a44ba08546
yaseenabdulsatar <112056637+yaseenabdulsatar@users.noreply.github.com>
2024-03-09 18:43:50 +0300

    Update setup_webserver.sh

--- solutions/wordpress-azure-vm-v1/scripts/setup_webserver.sh
+++ solutions/wordpress-azure-vm-v1/scripts/setup_webserver.sh
@@ -307,8 +307,8 @@ EOF
     fi
     if [ "$fileServerType" = "nfs" -o "$fileServerType" = "nfs-ha" -o "$fileServerType" = "nfs-byo" -o "$fileServerType" = "gluster" ]; then
         mkdir -p /var/www/html
-        rsync -av --delete /azlamp/html/. $htmlRootDir
-        #sudo ln -s /azlamp/html /var/www/html
+        #rsync -av --delete /azlamp/html/. $htmlRootDir
+        sudo ln -s /azlamp/html /var/www/html
         chown www-data:www-data -R $htmlRootDir && sync
         setup_html_local_copy_cron_job
      fi   
