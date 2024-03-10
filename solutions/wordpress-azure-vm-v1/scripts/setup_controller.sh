969d962ddef3ef781e5ce7ea670c46421d873cea
yaseenabdulsatar <112056637+yaseenabdulsatar@users.noreply.github.com>
2024-03-10 07:56:13 +0300

    Update setup_controller.sh

--- solutions/wordpress-azure-vm-v1/scripts/setup_controller.sh
+++ solutions/wordpress-azure-vm-v1/scripts/setup_controller.sh
@@ -138,10 +138,12 @@ function install_wordpress_application2 {
         local frontDoorFQDN=\$frontDoorFQDN
         local httpProtocol="http://"
         local wpHome="\$httpProtocol\$frontDoorFQDN"
-		echo "-------------------------------------"
+	
+	echo "-------------------------------------"
         read -p "Enter FQDN of the new website: " dnsSite
-        wpHome=\$dnsSite
+        wpHome="\$httpProtocol\$dnsSite"
         read -p "Title/name of the new website: " wpTitle
+        read -p "email address of the admin of the new website: " wpAdminEmail
         table_prefix=\$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
         # Creates a Database for CMS application
         #create_database \$dbIP \$dbadminloginazure \$dbadminpass \$applicationDbName \$wpDbUserId \$wpDbUserPass
