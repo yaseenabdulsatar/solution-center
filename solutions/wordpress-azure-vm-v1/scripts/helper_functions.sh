1e62da66c2640fd7d5bf656889d34e91ce952790
yaseenabdulsatar <112056637+yaseenabdulsatar@users.noreply.github.com>
2024-03-10 13:52:20 +0300

    Update helper_functions.sh

--- solutions/wordpress-azure-vm-v1/scripts/helper_functions.sh
+++ solutions/wordpress-azure-vm-v1/scripts/helper_functions.sh
@@ -727,13 +727,17 @@ server {
           fastcgi_buffers 16 16k;
           fastcgi_buffer_size 32k;
           fastcgi_param   SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-          # fastcgi_pass unix:/run/php/php${PhpVer}-fpm.sock;
+          fastcgi_pass unix:/run/php/php${PhpVer}-fpm.sock;
           fastcgi_pass backend;
           fastcgi_read_timeout 3600;
           fastcgi_index index.php;
           include fastcgi_params;
         }
 }
+EOF
+  fi
+
+  cat <<EOF >> /etc/nginx/sites-enabled/${siteFQDN}.conf
 server {
         listen 80;
 	      index index.php index.html index.htm;
@@ -900,6 +904,10 @@ server {
           include fastcgi_params;
         }
 }
+EOF
+  fi
+
+  cat <<EOF >> /etc/nginx/sites-enabled/${siteFQDN}.conf
 server {
         listen 80;
 	      index index.php index.html index.htm;
